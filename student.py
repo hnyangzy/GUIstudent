076c20f374665557633b316ab941e3ed99eb9b47
hnyangzy <43570632@qq.com>
2020-08-22 21:06:24 +0800

    zhe sce

--- student.py
+++ student.py
@@ -46,10 +46,59 @@ class AdminPage:
         self.admin_username=tk.Entry(self.window,width=30,font=tkFont.Font(size=14),bg='Ivory',show='*')
         self.admin_pass.pack()
         
-        Button(self.window,text='登录'，width=8,font=tkFont.Font(size=12),command=self.login).pack(pady=40)
-        Button(self.window,text='返回首页'，width=8,font=tkFont.Font(size=12),command=self.back).pack()
+        Button(self.window,text='登录',width=8,font=tkFont.Font(size=12),command=self.login).pack(pady=40)
+        Button(self.window,text='返回首页',width=8,font=tkFont.Font(size=12),command=self.back).pack()
         
         self.window.protocol('WM_DELETE_WINDOW',self.back)  #捕捉右上角关闭点击
         self.window.mainloop() #进入消息循环
         
+        
+    def login(self):
+        print(str(self.admin_username.get()))
+        print(str(self.admin_pass.get()))
+        admin_pass=None
+        
+        #数据库操作，查询管理员表
+        db=pymysql.connect('localhost','root','root','student') #打开数据库连接
+        cursor=db.cursor()  #使用cursor()方法获取操作游标
+        sql="SELECT * FROM admin_login_k where admin_id ='%s" % (self.admin_username.get())
+        try:
+            cursor.execute(sql)
+            
+            result = cursor.fetchall()
+            for row in result:
+                admin_id=row[0]
+                admin_pass=row[1]
+                print('admin_id=%s,admin_pass=%s'% (admin_id,amdin_pass))
+                
+        except:
+            print('错误：数据不正确!')
+            messagebox.showinfo('警告！','用户名或密码不正确！')
+        db.close()  #关闭数据库
+        
+        
+        print('正在登录管理员挂你界面')
+        print('self',self.admin_pass)
+        print('local',admin_pass)
+        
+        if self.admin_pass.get()==admin_pass:
+            AdminManage(self.window)   #进入管理员操作界面
+        else:
+            messagebox.showinfo('警告！','用户名或密码不正确！')
+            
+        def back(self):
+            StartPage(self.window)  #显示主窗口，销毁本窗口
+            
+#学生登录页面
+class StudentPage:
+    def __init__(self,parent_window):
+        parent_window.destroy()  #销毁主界面
+        
+        self.window=tk.TK()  #初始窗口声明
+        self.window.title('学生')
+            
+            
+            
+            
+                
         
