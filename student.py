c37860d8d42519ec9305de95b09a258bb1dd97c4
hnyangzy <43570632@qq.com>
2020-08-25 23:40:19 +0800

    2020-8-25

--- student.py
+++ student.py
@@ -42,8 +42,12 @@ class AdminPage:
         self.window.title('管理员登录页面')
         self.window.geometry('300x450')
         
-        label=tk.Lable(self.window,text='管理员登录',bg='green',font=tkFont.Font(size=14)).pack(pady=25)
-        self.admin_username=tk.Entry(self.window,width=30,font=tkFont.Font(size=14),bg='Ivory',show='*')
+        label=tk.Label(self.window,text='管理员姓名',bg='green',font=tkFont.Font(size=14)).pack(pady=25)
+        self.admin_username=tk.Entry(self.window,width=30,font=tkFont.Font(size=14),bg='Ivory')
+        self.admin_username.pack()
+        
+        label=tk.Label(self.window,text='管理员密码',bg='green',font=tkFont.Font(size=14)).pack(pady=25)
+        self.admin_pass=tk.Entry(self.window,width=30,font=tkFont.Font(size=14),bg='Ivory',show='*')
         self.admin_pass.pack()
         
         Button(self.window,text='登录',width=8,font=tkFont.Font(size=12),command=self.login).pack(pady=40)
@@ -56,20 +60,22 @@ class AdminPage:
     def login(self):
         print(str(self.admin_username.get()))
         print(str(self.admin_pass.get()))
+        
         admin_pass=None
         
         #数据库操作，查询管理员表
-        db=pymysql.connect('localhost','root','root','student') #打开数据库连接
+        db=pymysql.connect(host='localhost',user='root',password='123456',db='student',port=3306) #打开数据库连接
         cursor=db.cursor()  #使用cursor()方法获取操作游标
-        sql="SELECT * FROM admin_login_k where admin_id ='%s" % (self.admin_username.get())
+        sql="SELECT * FROM admin_login_k where admin_id ='%s'"% (self.admin_username.get())
         try:
+            db.ping(reconnect=True)
             cursor.execute(sql)
-            
             result = cursor.fetchall()
+            
             for row in result:
                 admin_id=row[0]
                 admin_pass=row[1]
-                print('admin_id=%s,admin_pass=%s'% (admin_id,amdin_pass))
+                #print('admin_id=%s,admin_pass=%s'% (admin_id,amdin_pass))
                 
         except:
             print('错误：数据不正确!')
@@ -77,16 +83,16 @@ class AdminPage:
         db.close()  #关闭数据库
         
         
-        print('正在登录管理员挂你界面')
+        print('正在登录管理员界面')
         print('self',self.admin_pass)
         print('local',admin_pass)
         
-        if self.admin_pass.get()==admin_pass:
-            AdminManage(self.window)   #进入管理员操作界面
+        if (self.admin_pass.get()==admin_pass):
+            AdminMange(self.window)   #进入管理员操作界面
         else:
             messagebox.showinfo('警告！','用户名或密码不正确！')
             
-        def back(self):
+    def back(self):
             StartPage(self.window)  #显示主窗口，销毁本窗口
             
 #学生登录页面
@@ -94,19 +100,19 @@ class StudentPage:
     def __init__(self,parent_window):
         parent_window.destroy()  #销毁主界面
         
-        self.window=tk.TK()  #初始窗口声明
+        self.window=tk.Tk()  #初始窗口声明
         self.window.title('学生登录')
         self.window.geometry('300x450')
         
-        label.window = tk.Label(self.window,text='学生登录',bg='green',font=tkFont.Font('Verdana',20),width=30,height=2)
+        label = tk.Label(self.window,text='学生登录',bg='green',font=tkFont.Font(size=30),width=30,height=2)
         label.pack()
         
-        label(self.window,text='学生账号：',font=tkFont.Font(size=14)).pack(pady=25)
+        label=tk.Label(self.window,text='学生账号：',font=tkFont.Font(size=14)).pack(pady=25)
         self.student_id=tk.Entry(self.window,width=30,font=tkFont.Font(size=14),bg='Ivory')
         self.student_id.pack()
         
-        label(self.window, text='学生密码：',font=tkFont.Font(size=(14))).pack(pady=25)
-        self.student_pass=tk.Entry(self.window,width=30,font=tkFont.Font(size=14),bg='Ivory')
+        label=tk.Label(self.window, text='学生密码：',font=tkFont.Font(size=(14))).pack(pady=25)
+        self.student_pass=tk.Entry(self.window,width=30,font=tkFont.Font(size=14),bg='Ivory',show=('*'))
         self.student_pass.pack()
         
         
@@ -123,10 +129,11 @@ class StudentPage:
         stu_pass=None
         
         #数据库操作 查询学生表
-        db=pymysql.connect('localhost','root','root','student')  #打开数据库连接
+        db=pymysql.connect(host='localhost',user='root',password='123456',db='student')  #打开数据库连接
         cursor=db.cursor()  #使用cursor()方法获得操作游标
         sql="select * from stu_login_k where stu_id='%s'"% (self.student_id.get())
         try:
+            db.ping(reconnect=True)
             cursor.execute(sql)
             results = cursor.fetchall()
             for row in results:
@@ -152,7 +159,7 @@ class StudentPage:
             
             
     def back(self):
-        StarPage(self.window)  #显示主窗口 销毁本窗口
+        StartPage(self.window) #显示主窗口 销毁本窗口
       
         
 #管理员操作界面
@@ -195,7 +202,7 @@ class AdminMange:
         self.age = []
         
         #打开数据库连接
-        db=pymysql.connect('localhost','root','root','student')
+        db=pymysql.connect(host='localhost',user='root',password='123456',db='student')
         cursor=db.cursor()
         sql="select * from student_k"  
         try:
@@ -230,8 +237,8 @@ class AdminMange:
         #定义顶部区域
         #定义左上方区域
         
-        self.top.title=Label(self.frame_left_top,text='学生信息：',font=('Verdana',20))
-        self.top.title.grid(row=0,column=0,columnspan=2, sticky=NSEW,padx=50, pady=10)
+        self.top_title=Label(self.frame_left_top,text='学生信息：',font=('Verdana',20))
+        self.top_title.grid(row=0,column=0,columnspan=2, sticky=NSEW,padx=50, pady=10)
         self.left_top_frame=tk.Frame(self.frame_left_top)
         self.var_id=StringVar() #声明学号
         self.var_name=StringVar()
@@ -247,19 +254,19 @@ class AdminMange:
         
         #姓名
         
-        self.right_top_name_label=Lbael(self.frame_left_top,text='姓名：',font=('Verdana',15))
+        self.right_top_name_label=Label(self.frame_left_top,text='姓名：',font=('Verdana',15))
         self.right_top_name_entry=Entry(self.frame_left_top,textvariable=self.var_name,font=('Verdana',15))
         self.right_top_name_label.grid(row=2,column=0)
         self.right_top_name_entry.grid(row=2,column=1)
         
         #性别
-        self.right_top_gender_label=Lbael(self.frame_left_top,text='性别：',font=('Verdana',15))
+        self.right_top_gender_label=Label(self.frame_left_top,text='性别：',font=('Verdana',15))
         self.right_top_gender_entry=Entry(self.frame_left_top,textvariable=self.var_gender, font=('Verdana',15))
         self.right_top_gender_label.grid(row=3,column=0)
         self.right_top_gender_entry.grid(row=3,column=1)
         
         #年龄
-        self.right_top_age_label=Lbael(self.frame_left_top,text='年龄：',font=('Verdana',15))
+        self.right_top_age_label=Label(self.frame_left_top,text='年龄：',font=('Verdana',15))
         self.right_top_age_entry=Entry(self.frame_left_top,textvariable=self.var_age, font=('Verdana',15))
         self.right_top_age_label.grid(row=4,column=0)
         self.right_top_age_entry.grid(row=4,column=1)
@@ -270,15 +277,16 @@ class AdminMange:
         self.tree.bind('<Button-1>', self.click)  #左键获取位置
         self.right_top_button1=ttk.Button(self.frame_right_top,text='新建学生信息',width=20,command=self.new_row)
         self.right_top_button2=ttk.Button(self.frame_right_top,text='更新选中学生信息',width=20,command=self.updata_row)
-        self.rigth_top_button3=ttk.Buttoon(self.frame_right_top,text='删除选中学生信息',width=20,command=self.delete_row)
-        
+        self.right_top_button3=ttk.Button(self.frame_right_top,text='删除选中学生信息',width=20,command=self.del_row)
+        self.right_top_button4=ttk.Button(self.frame_right_top,text='返回首页',width=20,command=self.back)
         
         #位置设置
         
         self.right_top_title.grid(row=1,column=0,pady=10)
-        self.right_top_button1.grid(row=2,column=0,padx=20,pady=10)
-        self.right_top_button2.grid(row=3,column=0,padx=20,pady=10)
-        self.right_top_button3.grid(row=4,column=0,padx=20,pady=10)  
+        self.right_top_button1.grid(row=2,column=0,padx=20,pady=5)
+        self.right_top_button2.grid(row=3,column=0,padx=20,pady=5)
+        self.right_top_button3.grid(row=4,column=0,padx=20,pady=5)
+        self.right_top_button4.grid(row=5,column=0,padx=20,pady=5)
         
         #整体区域定位
         
@@ -299,12 +307,11 @@ class AdminMange:
         self.frame_center.tkraise()        
         self.frame_bottom.tkraise()
         
-        self.window.protocol('wm_DELETE_WINDOW',self.back)
-        
-        self.window.mainloop()
-        
+        self.window.protocol("WM_DELETE_WINDOW", self.back)  # 捕捉右上角关闭点击
+        self.window.mainloop()  # 进入消息循环
+ 
     def back(self):
-        StartPage(self.window)  
+        StartPage(self.window) 
             
             
             
@@ -334,25 +341,29 @@ class AdminMange:
             
     
     def new_row(self):
-        print('123')
-        print(self.var_id.get())
-        print(self.id)
+        #print('123')
+        #print(self.var_id.get())
+        #print(self.var_name.get())
+        #print(self.var_gender.get())
+        #print(self.var_age.get())
+        #print(self.id)
         if str(self.var_id.get()) in self.id:
         	messagebox.showinfo('警告！', '该学生已存在！')
-        if self.var_id.get() != '' and self.var_name.get() != '' and self.var_gender.get() != '' and self.var_age.get() != '':
-				# 打开数据库连接
-            pymysql.connect("localhost", "root", "root", "student")
-            cursor = db.cursor()  # 使用cursor()方法获取操作游标
-            sql="INSERT INTO student_k(id, name, gender, age) \
-				       VALUES ('%s', '%s', '%s', '%s')" % \
-					  (self.var_id.get(), self.var_name.get(), self.var_gender.get(), self.var_age.get())  # SQL 插入语句
-            try:
+        else:
             
-                cursor.execute(sql)  # 执行sql语句
-                db.commit()  # 提交到数据库执行
-            except:
-                db.rollback()  # 发生错误时回滚
-                messagebox.showinfo('警告！', '数据库连接失败！')
+            if self.var_id.get() != '' and self.var_name.get() != '' and self.var_gender.get() != '' and self.var_age.get() != '':
+				# 打开数据库连接
+                pymysql.connect(host='localhost',user='root',password='123456',db='student',charset='utf8')
+                cursor = db.cursor()  # 使用cursor()方法获取操作游标
+                sql="insert into student_k (id,name,gender,age) values ('%s','%s','%s','%s')" %(self.var_id.get(),self.var_name.get(),self.var_gender.get(),self.var_age.get())  # SQL 插入语句
+                try:
+                    
+                    db.ping(reconnect=True)
+                    cursor.execute(sql)  # 执行sql语句
+                    db.commit()  # 提交到数据库执行
+                except:
+                    db.rollback()  # 发生错误时回滚
+                    messagebox.showinfo('警告！', '数据库连接失败！')
                 db.close()  # 关闭数据库连接
  
                 self.id.append(self.var_id.get())
@@ -364,35 +375,39 @@ class AdminMange:
                 self.age[len(self.id) - 1]))
                 self.tree.update()
                 messagebox.showinfo('提示！', '插入成功！')
-        else:
-            messagebox.showinfo('警告！', '请填写学生数据')
+            else:
+                messagebox.showinfo('警告！', '请填写学生数据')
  
     def updata_row(self):
         res = messagebox.askyesnocancel('警告！', '是否更新所填数据？')
         if res == True:
-            if self.var_id.get() == self.row_info[0]:  # 如果所填学号 与 所选学号一致
+            if (self.var_id.get() == self.row_info[0]):  # 如果所填学号 与 所选学号一致
             # 打开数据库连接
-                db = pymysql.connect("localhost", "root", "root", "student")
+                db = pymysql.connect(host="localhost", user="root", password="123456", db="student")
                 cursor = db.cursor()  # 使用cursor()方法获取操作游标
                 sql = "UPDATE student_k SET name = '%s', gender = '%s', age = '%s' \
                     WHERE id = '%s'" % (self.var_name.get(), self.var_gender.get(), self.var_age.get(), self.var_id.get())  # SQL 插入语句
                 try:
+                    db.ping(reconnect=True)
                     cursor.execute(sql)  # 执行sql语句
                     db.commit()  # 提交到数据库执行
-                    messagebox.showinfo('提示！', '更新成功！')
+                    #messagebox.showinfo('提示！', '更新成功！')
+                    #self.tree.update()
                 except:
                     db.rollback()  # 发生错误时回滚
                     messagebox.showinfo('警告！', '更新失败，数据库连接失败！')
-                    db.close()  # 关闭数据库连接
+                db.close()  # 关闭数据库连接
  
-                    id_index = self.id.index(self.row_info[0])
-                    self.name[id_index] = self.var_name.get()
-                    self.gender[id_index] = self.var_gender.get()
-                    self.age[id_index] = self.var_age.get()
+                id_index = self.id.index(self.row_info[0])
+                self.name[id_index] = self.var_name.get()
+                self.gender[id_index] = self.var_gender.get()
+                self.age[id_index] = self.var_age.get()
  
-                    self.tree.item(self.tree.selection()[0], values=(
-                    self.var_id.get(), self.var_name.get(), self.var_gender.get(),
-                    self.var_age.get()))  # 修改对于行信息
+                self.tree.item(self.tree.selection()[0], values=(
+                self.var_id.get(), self.var_name.get(), self.var_gender.get(),
+                self.var_age.get()))  # 修改对于行信息
+                self.tree.update()
+                messagebox.showinfo('提示！', '更新成功！')
             else:
                 messagebox.showinfo('警告！', '不能修改学生学号！')
  
@@ -403,9 +418,9 @@ class AdminMange:
             print(self.tree.selection()[0])  # 行号
             print(self.tree.get_children())  # 所有行
             # 打开数据库连接
-            db = pymysql.connect("localhost", "root", "root", "student")
+            db = pymysql.connect(host="localhost", user="root", password="123456", db="student")
             cursor = db.cursor()  # 使用cursor()方法获取操作游标
-            sql = "DELETE FROM student_k WHERE id = '%s'" % (self.row_info[0]) # SQL 插入语句
+            sql = "DELETE FROM student_k WHERE id = '%s'" % (self.row_info[0]) # SQL 删除语句
             try:
                 cursor.execute(sql)  # 执行sql语句
                 db.commit()  # 提交到数据库执行
@@ -424,54 +439,59 @@ class AdminMange:
                 print(self.id)
                 self.tree.delete(self.tree.selection()[0])  # 删除所选行
                 print(self.tree.get_children())
+                
+    
  
  
 # 学生查看信息界面
 class StudentView:
-	def __init__(self, parent_window, student_id):
-		parent_window.destroy() # 销毁主界面
+    def __init__(self, parent_window, student_id):
+        parent_window.destroy() # 销毁主界面
  
-		self.window = tk.Tk()  # 初始框的声明
-		self.window.title('关于')
-		self.window.geometry('300x450')  # 这里的乘是小x
+        self.window = tk.Tk()  # 初始框的声明
+        self.window.title('关于')
+        self.window.geometry('300x450')  # 这里的乘是小x
  
-		label = tk.Label(self.window, text='学生信息查看', bg='green', font=('Verdana', 20), width=30, height=2)
-		label.pack(pady=20)
+        label = tk.Label(self.window, text='学生信息查看', bg='green', font=('Verdana', 20), width=30, height=2)
+        label.pack(pady=20)
  
-		self.id = '学号:' + ''
-		self.name = '姓名:' + ''
-		self.gender = '性别:' + ''
-		self.age = '年龄:' + ''
+        self.id = '学号:' + ''
+        self.name = '姓名:' + ''
+        self.gender = '性别:' + ''
+        self.age = '年龄:' + ''
 		# 打开数据库连接
-		db = pymysql.connect("localhost", "root", "root", "student")
-		cursor = db.cursor()# 使用cursor()方法获取操作游标
-		sql = "SELECT * FROM student_k WHERE id = '%s'" % (student_id) # SQL 查询语句
-		try:
+        print(student_id)
+        db = pymysql.connect(host="localhost",user="root", password="123456", db="student")
+        cursor = db.cursor()# 使用cursor()方法获取操作游标
+        sql = "SELECT * FROM student_k WHERE id = '%s'" % (student_id) # SQL 查询语句
+        try:
 			# 执行SQL语句
-			cursor.execute(sql)
+            db.ping(reconnect=True)
+            cursor.execute(sql)
 			# 获取所有记录列表
-			results = cursor.fetchall()
-			for row in results:
-				self.id = '学号:' + row[0]
-				self.name = '姓名:' + row[1]
-				self.gender = '性别:' + row[2]
-				self.age = '年龄:' + row[3]
-		except:
-			print("Error: unable to fetch data")
-		db.close()		# 关闭数据库连接
+            results = cursor.fetchall()
+            for row in results:
+                self.id = '学号:' + row[0]
+                self.name = '姓名:' + row[1]
+                self.gender = '性别:' + row[2]
+                self.age = '年龄:' + row[3]
+        except:
+            print("Error: unable to fetch data")
+            self.messagebox.showinfo('出错！')
+        db.close()		# 关闭数据库连接
  
-		Label(self.window, text=self.id, font=('Verdana', 18)).pack(pady=5)
-		Label(self.window, text=self.name, font=('Verdana', 18)).pack(pady=5)
-		Label(self.window, text=self.gender, font=('Verdana', 18)).pack(pady=5)
-		Label(self.window, text=self.age, font=('Verdana', 18)).pack(pady=5) 
+        Label(self.window, text=self.id, font=('Verdana', 18)).pack(pady=5)
+        Label(self.window, text=self.name, font=('Verdana', 18)).pack(pady=5)
+        Label(self.window, text=self.gender, font=('Verdana', 18)).pack(pady=5)
+        Label(self.window, text=self.age, font=('Verdana', 18)).pack(pady=5) 
 
-		Button(self.window, text="返回首页", width=8, font=tkFont.Font(size=16), command=self.back).pack(pady=25)
+        Button(self.window, text="返回首页", width=8, font=tkFont.Font(size=16), command=self.back).pack(pady=25)
  
-		self.window.protocol("WM_DELETE_WINDOW", self.back)  # 捕捉右上角关闭点击
-		self.window.mainloop()  # 进入消息循环
+        self.window.protocol("WM_DELETE_WINDOW", self.back)  # 捕捉右上角关闭点击
+        self.window.mainloop()  # 进入消息循环
  
-	def back(self):
-		StartPage(self.window)  # 显示主窗口 销毁本窗口
+    def back(self):
+        StartPage(self.window)  # 显示主窗口 销毁本窗口
  
  
 # About页面
@@ -486,7 +506,7 @@ class AboutPage:
 		label = tk.Label(self.window, text='学生信息管理系统', bg='green', font=('Verdana', 20), width=30, height=2)
 		label.pack()
  
-		Label(self.window, text='作者：清晨的光明', font=('Verdana', 18)).pack(pady=30)
+		Label(self.window, text='作者：十四的月亮', font=('Verdana', 18)).pack(pady=30)
 		Label(self.window, text='blog.csdn.net/kdongyi', font=('Verdana', 18)).pack(pady=5)
  
 		Button(self.window, text="返回首页", width=8, font=tkFont.Font(size=12), command=self.back).pack(pady=100)
@@ -501,7 +521,7 @@ class AboutPage:
 if __name__ == '__main__':
 	try:
 		# 打开数据库连接 连接测试
-		db = pymysql.connect("localhost", "root", "root", "student")
+		db = pymysql.connect(host='localhost',user= 'root',password='123456', db='student',port=3306)
 		# 使用cursor()方法获取操作游标
 		cursor = db.cursor()
 		# 如果数据表不存在则创建表 若存在则跳过
